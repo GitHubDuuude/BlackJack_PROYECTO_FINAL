a04050f6a01aadbfe724d1d158e0348e8776a8c6
GitHubDuuude <juandapar16@gmail.com>
2025-05-12 21:01:02 -0500

    Adición del panel del juego principal / botones / MainPanel.java / MainFrame.java

--- BlackJack_PROYECTO_FINAL/src/main/java/co/edu/uptc/view/MainFrame.java
+++ BlackJack_PROYECTO_FINAL/src/main/java/co/edu/uptc/view/MainFrame.java
@@ -1,4 +1,26 @@
 package co.edu.uptc.view;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    private MainPanel mainPanel;
+
+
+    public MainFrame() {
+        super("Blackjack!");
+        initComponents();
+        setSize(1400, 1000);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void initComponents() {
+        mainPanel = new MainPanel();
+        setContentPane(mainPanel);
+    }
+
+    public static void main(String[] args) {
+        new MainFrame();
+    }
 }
